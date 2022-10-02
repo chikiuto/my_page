4429d9a91477f33927f1c0f01e7abd88c98d080b
chikiuto <abetokichiro@gmail.com>
2022-10-02 22:27:54 +0900

    accept

--- db/migrate/20220928094537_create_posts.rb
+++ db/migrate/20220928094537_create_posts.rb
@@ -4,14 +4,9 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.string :name, null: false
       t.text :question, null: false
       t.text :answer
-<<<<<<< HEAD
       # t.string :statusn, null:false, default: "published", comment: "published: 公開"
       # enum
       t.boolean :is_published, null: false, default: false
-=======
-      t.boolean :is_published, null: false, default: false
-
->>>>>>> refs/remotes/origin/main
       t.timestamps
     end
   end
